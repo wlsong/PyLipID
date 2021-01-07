bb4a6beb4e54f01e15ca168b194102665b11e1e0
Wanling Song <wlsong@users.noreply.github.com>
2021-01-07 14:09:21 +0000

    Update pylipid.py

--- pylipid.py
+++ pylipid.py
@@ -1078,11 +1078,11 @@ for bs_id in np.arange(num_of_binding_site):
                 fig, ax = plt.subplots(1, 1, figsize=(3.5, 1.5))
                 ax.xaxis.set_major_locator(MultipleLocator(10))
                 ax.xaxis.set_minor_locator(MultipleLocator(1))
-            elif len(df) <= 500 and len(df) > 50:
+            elif 50 < len(df) <= 500:
                 fig, ax = plt.subplots(1, 1, figsize=(5.5, 1.8))
                 ax.xaxis.set_major_locator(MultipleLocator(50))
                 ax.xaxis.set_minor_locator(MultipleLocator(10))
-            elif len(df) > 500 and len(df) <= 2000:
+            elif 500 < len(df) <= 2000:
                 fig, ax = plt.subplots(1, 1, figsize=(7.5, 2.3))
                 ax.xaxis.set_major_locator(MultipleLocator(200))
                 ax.xaxis.set_minor_locator(MultipleLocator(50))
