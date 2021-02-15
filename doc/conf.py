025c2727d6626cbb328393b17e48c33ecc3c1f6c
Wanling Song <wlsonglinda@gmail.com>
2021-02-15 22:00:20 +0000

    minor changes

--- doc/conf.py
+++ doc/conf.py
@@ -45,23 +45,20 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
-# html_theme = 'alabaster'
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['static']
-
-# The name of an image file (relative to this directory) to place at the top
-# of the sidebar.
 html_logo = 'static/pylipid_logo.png'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+    'prev_next_buttons_location': 'bottom',
+    'style_nav_header_background': 'white',
+}
 
 exclude_patterns = ['_build', '*_test*', '**/.ipynb_checkpoints/*']
 
